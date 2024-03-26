51389f9a94e38e1a86dcd3069c4b5356f1204d00
MariBayd <masha.baydina@gmail.com>
2024-03-27 02:24:52 +0300

    buttons refactoring, add transition (#1)

---
+++ gh-pages/script.js
@@ -0,0 +1,13 @@
+/**
+ * download pdf
+ */
+function downloadPdfClick() {
+    document.getElementById('download-link').click();
+}
+
+/**
+ * follow the link to GitHub 
+ */
+function goToSource() {
+    window.location.href='https://github.com/Deligor6321/cv/';
+}
